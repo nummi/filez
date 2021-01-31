1c600d5bff8455104e774ae5c80c1af815369419
Jerry Nummi <jerrynummi@me.com>
2021-01-31 12:19:23 -0800

    First

---
+++ next.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  images: {
+    domains: [`${process.env.S3_UPLOAD_BUCKET}.s3.amazonaws.com`],
+  },
+}
--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,46 @@
+import { useState } from 'react'
+import { useS3Upload, getImageData } from 'next-s3-upload'
+import Image from 'next/image'
+
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
+  let [imageUrl, setImageUrl] = useState()
+  let [height, setHeight] = useState()
+  let [width, setWidth] = useState()
+  let { FileInput, openFileDialog, uploadToS3 } = useS3Upload()
+
+  let handleFileChange = async (file) => {
+    let { url } = await uploadToS3(file)
+    let { height, width } = await getImageData(file)
+    setWidth(width)
+    setHeight(height)
+    setImageUrl(url)
+  }
+
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Upload Files</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+      <div>
+        <FileInput onChange={handleFileChange} />
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+        <button onClick={openFileDialog}>Upload file</button>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+        {imageUrl && (
+          <div>
+            <Image src={imageUrl} width={width} height={height} />
+            <div>{imageUrl}</div>
+            <div>
+              {height}x{width}
+            </div>
+          </div>
+        )}
+      </div>
     </div>
   )
 }
